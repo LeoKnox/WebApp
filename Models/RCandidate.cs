95b4109ac0cb0cf809ced087077535950e484a96
LeoKnox <LeoKnox@users.noreply.github.com>
2020-07-22 17:29:09 -0700

    Add files via upload

---
+++ Models/RCandidate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Models
+{
+    public class RCandidate
+    {
+        [Key]
+        public int id { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string roomName { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string floor { get; set; }
+        public int length { get; set; }
+        public int width { get; set; }
+    }
+}

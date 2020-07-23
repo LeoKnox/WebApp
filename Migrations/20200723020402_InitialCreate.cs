c544f4fe9679d64b197752ba72f4dd90d9421dbb
LeoKnox <LeoKnox@users.noreply.github.com>
2020-07-22 19:19:18 -0700

    Add files via upload

---
+++ Migrations/20200723020402_InitialCreate.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApp.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "RCandidate",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    roomName = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    floor = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    length = table.Column<int>(nullable: false),
+                    width = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RCandidate", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RCandidate");
+        }
+    }
+}

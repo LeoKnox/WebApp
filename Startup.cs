76dadc1114e74b4ea77974108433014108ce3ff7
LeoKnox <LeoKnox@users.noreply.github.com>
2020-07-23 14:04:07 -0700

    Add files via upload

--- Startup.cs
+++ Startup.cs
@@ -30,7 +30,7 @@ namespace WebApp
             services.AddControllers();
 
             services.AddDbContext<RoomDBContext>(options =>
-            options.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
+                options.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
